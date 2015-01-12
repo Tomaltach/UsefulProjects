81c0d4429aab132b8916fb3d34e3aff13c98c4d3
Tomaltach <thomas.donegan@mycit.ie>
2015-01-12 14:59:39 +0000

    Using a tabbed pane.

--- TimeKeeper/src/main/java/ie/tom/timekeeper/gui/GUI.java
+++ TimeKeeper/src/main/java/ie/tom/timekeeper/gui/GUI.java
@@ -1,12 +1,17 @@
 package ie.tom.timekeeper.gui;
 
+import java.awt.BorderLayout;
+
 import ie.tom.timekeeper.gui.panel.AddPanel;
 import ie.tom.timekeeper.gui.panel.Panel;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class GUI extends JFrame {
+	@SuppressWarnings("unused")
 	private String user;
 
 	public GUI(String user) {
@@ -23,10 +28,15 @@ public class GUI extends JFrame {
 		setVisible(true);
 	}
 	private void build() {
-		buildAddPanel();		
+		JPanel panel = new JPanel();
+		JTabbedPane tabbed = new JTabbedPane();
+		tabbed.addTab("Add", buildAddPanel());
+		
+		panel.add(tabbed, BorderLayout.CENTER);
+		add(panel);
 	}
-	private void buildAddPanel() {
+	private JPanel buildAddPanel() {
 		Panel addPanel = new AddPanel();
-		add(addPanel.createPanel());		
+		return addPanel.createPanel();		
 	}
 }
