37a9c04599be5eb0339a36ee25865e4554b5c77c
Tomaltach <thomas.donegan@mycit.ie>
2015-01-15 09:56:50 +0000

    JClendarButton has been added but, I am now trying to make the txtDate JTextField clickable rather than have an extra button added as it makes the layout messing. I could always make 2 rows of input rather than 1.

--- TimeKeeper/src/main/java/ie/tom/timekeeper/gui/panel/AddPanel.java
+++ TimeKeeper/src/main/java/ie/tom/timekeeper/gui/panel/AddPanel.java
@@ -4,6 +4,12 @@ import ie.tom.timekeeper.gui.panel.feature.PromptTextField;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.DateFormat;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -14,17 +20,27 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import net.java.dev.designgridlayout.DesignGridLayout;
+import net.sourceforge.jcalendarbutton.JCalendarButton;
 
+@SuppressWarnings("deprecation")
 public class AddPanel implements Panel {
 	private final static String[] TYPE = {"Walk", "Jog", "Run", "Sprint", "Cycle"};
 	private final static String[] UNIT = {"Kilometer", "Mile"}; 
+	private final static String DATE_PATTERN = "dd/MM/yyyy";
+	public static DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+	
+	private JTextField txtDate;
+	private JCalendarButton btnCalendar;
 
 	@Override
 	public JPanel createPanel() {
 		JLabel lblDate = new JLabel("Date");
-		final JTextField txtDate = new JTextField(processDate(), 20);
+		txtDate = new JTextField(currentDate(), 20);
+		btnCalendar = new JCalendarButton();
 		JLabel lblTime = new JLabel("Time");
 		final PromptTextField txtTime = new PromptTextField("hh:mm:ss:ms");
 		JLabel lblDistance = new JLabel("Distance");
@@ -35,19 +51,50 @@ public class AddPanel implements Panel {
 		final JTextArea taOutput = new JTextArea(20, 5);
 		JScrollPane scrollOutput = new JScrollPane(taOutput);
 		
+		txtDate.addMouseListener(new MouseListener() {
+			@Override
+		    public void mouseClicked(MouseEvent e) {
+
+		    }
+			@Override
+			public void mouseEntered(MouseEvent arg0) {}
+			@Override
+			public void mouseExited(MouseEvent arg0) {}
+			@Override
+			public void mousePressed(MouseEvent arg0) {}
+			@Override
+			public void mouseReleased(MouseEvent arg0) {}
+		});
+		btnCalendar.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+            	dateOnlyPopupChanged(evt);
+            }
+        });
 		btnAddRecord.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String record = "";
-				record += txtDate.getText() + ", ";
-				record += txtTime.getText() + ", ";
-				record += txtDistance.getText() + ", ";
-				record += cmbType.getSelectedItem() + ", ";
-				record += cmbUnit.getSelectedItem();
+				if(txtDate.getText().equals("") || checkDate(txtDate.getText()) == false) {
+					record += "Fill in date!\n";
+				}
+				if(txtTime.getText().equals("hh:mm:ss:ms")) {
+					record += "Fill in time!\n";
+				}
+				if(txtDistance.getText().equals("")) {
+					record += "Fill in distance!\n";
+				} 
+				if(record.equals("")) {
+					record += txtDate.getText() + ", ";
+					record += txtTime.getText() + ", ";
+					record += txtDistance.getText() + ", ";
+					record += cmbType.getSelectedItem() + ", ";
+					record += cmbUnit.getSelectedItem();
+				}
 				
 				taOutput.append(record);
 				System.out.println(record);
 			}
 		});
+		txtDate.setEditable(false);
 		taOutput.setEditable(false);
 		cmbType.setSelectedIndex(1);
 		
@@ -59,16 +106,49 @@ public class AddPanel implements Panel {
 		
 		return panel;		
 	}
-	private String processDate() {
+	private String currentDate() {
 		String date = "";
 		LocalDate today;
 		try {
-			today = LocalDate.now();
-			
+			today = LocalDate.now();			
 		} catch(Exception e) {
 			return "Number!";
 		}
 		date = today.getDayOfMonth() + "/" + today.getMonthOfYear() + "/" + today.getYear();
 		return date;
 	}
+	private boolean checkDate(String date) {
+		try {
+			DateTimeFormatter dtf = DateTimeFormat.forPattern(DATE_PATTERN);
+			dtf.parseDateTime(date);
+			return true;
+		} catch (IllegalArgumentException iae) {
+			return false;
+		}
+	}
+	private void dateOnlyPopupChanged(PropertyChangeEvent evt) {
+		if (evt.getNewValue() instanceof Date) {
+			setDate((Date)evt.getNewValue());
+		}
+    }
+	public void setDate(String dateString) {
+		Date date = null;
+		try {
+			if((dateString != null) && (dateString.length() > 0)) {
+				date = dateFormat.parse(dateString);
+			}
+		}
+		catch(Exception e) {
+			date = null;
+		}
+		this.setDate(date);
+	}
+	public void setDate(Date date){
+		String dateString = "";
+		if(date != null) {
+			dateString = dateFormat.format(date);
+		}
+		txtDate.setText(dateString);
+		btnCalendar.setTargetDate(date);
+    }
 }
